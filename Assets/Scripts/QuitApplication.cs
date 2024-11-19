491b151b4669a3988ade105dc316bc5d4c7388c2
Andrew <andrew@responsiveads.com>
2024-11-18 17:11:44 -0800

    Added another level and changed the terrain a bit. Finally, I added a script to quit the application. This will be the last push for this game because I'm moving on to something else!

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("escape"))
+        {
+            Debug.Log("Escape");
+            Application.Quit();
+        }
+    }
+}
